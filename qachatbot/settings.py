107a6f97109e2168a9a8bf346d13c4913d5524c1
SamarthDhage <samarthdhage44@gmail.com>
2025-01-24 17:43:35 +0530

    third

--- qachatbot/settings.py
+++ qachatbot/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-g8lp=2ymwnr5!aea@)qga*p@vr8urv4yx&9#db@d0yo-hecf4h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
